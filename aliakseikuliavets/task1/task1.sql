3a980c87351355a4db6406b3b7358eb4feb295fc
Aliaksei <kulevetsav@gmail.com>
2023-08-22 11:38:12 +0200

    fix 1

--- aliakseikuliavets/task1/task1.sql
+++ aliakseikuliavets/task1/task1.sql
@@ -1,1 +1,10 @@
--- code
+-- Вывести страну, которая на пятом месте по количеству клиентов
+SELECT
+	Country,
+	COUNT(*) AS total_customers
+FROM Customers
+
+GROUP BY Country
+
+ORDER BY total_customers DESC
+LIMIT 1 OFFSET 4
