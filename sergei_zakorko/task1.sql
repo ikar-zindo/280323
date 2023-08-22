5edccf39689c9eaa4a7a1d1ed0d79c673e6ebedd
Zakorko Serhii <rkot.vet@gmail.com>
2023-08-22 11:36:30 +0200

    Task1

--- sergei_zakorko/task1.sql
+++ sergei_zakorko/task1.sql
@@ -1,9 +1,9 @@
 --Вывести страну, которая на пятом месте по количеству клиентов--
 
 SELECT
-   count(*) as count,
-   country
-FROM customers
-GROUP BY customerID
-ORDER BY count DESC
+	Country,
+	COUNT(*) AS total_customers
+FROM Customers
+GROUP BY Country
+ORDER BY total_customers DESC
 LIMIT 1 offset 4
