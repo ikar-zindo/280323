e9c9046f8231429e340bd1b15f229725e545eddf
Zakorko Serhii <rkot.vet@gmail.com>
2023-09-05 12:36:54 +0200

    added FOREIGN KEYS

--- sergei_zakorko/task5.sql
+++ sergei_zakorko/task5.sql
@@ -5,4 +5,6 @@ CREATE TABLE IF NOT EXISTS orders (
   	emploee_id int NOT null
 	total_cost float not null,
 	is_completed bool DEFAULT false
+    FOREIGN KEY (emploee_id) REFERENCES employes(emploee_id)
+    FOREIGN KEY (customer_name) REFERENCES customers(customer_name)
 	);
