29855c9077c8e2acb3544aaef0dd5032608fdbe2
ikar-zindo <plag.ikar@gmail.com>
2023-09-05 12:25:05 +0200

    add FOREIGN KEY to create_table_orders.sql

--- ivanbukrieiev/create_table_orders.sql
+++ ivanbukrieiev/create_table_orders.sql
@@ -1,6 +1,9 @@
 CREATE TABLE orders (
-  order_id int(11) primary key AUTO_INCREMENT,
+  order_id int(11) primary key auto_increment,
   user_id int(11) NOT NULL,
   employee_id int(11) NOT NULL,
   created_at timestamp default current_timestamp,
-)
+);
+
+ALTER TABLE orders
+ADD FOREIGN KEY (user_id) REFERENCES users(user_id);
