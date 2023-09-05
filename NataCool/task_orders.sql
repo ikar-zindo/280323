ada9e8b9fb13806fbd7570703c15f920037acb7a
Natacool <natacool812@gmail.com>
2023-09-05 12:38:30 +0200

    Update task_orders.sql

--- NataCool/task_orders.sql
+++ NataCool/task_orders.sql
@@ -1,1 +1,13 @@
 -- comment
+
+
+CREATE TABLE IF NOT EXISTS orders (
+	order_id int primary key auto_increment,
+	order_date timestamp default current_timestamp
+
+);
+
+
+
+ALTER TABLE orders
+ADD FOREIGN KEY ----() REFERENCES ------();
