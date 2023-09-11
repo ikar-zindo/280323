435c428d0b5e907134cab830a1ea9c4b2d874d23
Natacool <natacool812@gmail.com>
2023-09-11 17:18:15 +0200

    modified

--- NataCool/task_orders.sql
+++ NataCool/task_orders.sql
@@ -3,7 +3,11 @@
 
 CREATE TABLE IF NOT EXISTS orders (
 	order_id int primary key auto_increment,
-	order_date timestamp default current_timestamp
+	order_date timestamp default current_timestamp,
+    user_id int, 
+	shipment_id int, 
+	emplyee_id int, 
+	order_details varchar(64)
 
 );
 
